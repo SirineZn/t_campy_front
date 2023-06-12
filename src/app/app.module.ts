1a97bde97fba1e720ef1d185b77c30a1c00378d0
maxstain <firaschabchoub@hotmail.com>
2023-06-12 16:46:12 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ForumComponent } from './Views/forum/forum.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './Views/home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path:'', redirectTo:'/home', pathMatch:'full'},
+  { path:'forum', component: ForumComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
