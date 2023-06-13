c6647af41c30ea34809c5f0a623a2d37cfa49e21
Firas Chabchoub <firaschabchoub@hotmail.com>
2023-06-14 00:44:22 +0100

    [MODIFY]: Working on the forums

--- src/app/Components/questions-list/questions-list.component.ts
+++ src/app/Components/questions-list/questions-list.component.ts
@@ -8,8 +8,8 @@ import { Topic } from '../Utils/chat-card/Topic.model';
 })
 export class QuestionsListComponent {
   public topics: Topic[] = [
-        new Topic( '1', 'Title 1', 'Description 1', new Date(), 'Author 1', ['tag1', 'tag2'], 1, 2, 'Category 1'),
-        new Topic( '2', 'Title 2', 'Description 2', new Date(), 'Author 2', ['tag1', 'tag2'], 1, 2, 'Category 2'),
-        new Topic( '3', 'Title 3', 'Description 3', new Date(), 'Author 3', ['tag1', 'tag2'], 1, 2, 'Category 3'),
+        new Topic( '1', 'Title 1', 'Description 1', new Date(), 'Author 1', ['General', 'Camping'], 1, 2, 'Camping'),
+        new Topic( '2', 'Title 2', 'Description 2', new Date(), 'Author 2', ['Ideas', 'General'], 1, 2, 'Camping'),
+        new Topic( '3', 'Title 3', 'Description 3', new Date(), 'Author 3', ['Offer'], 1, 2, 'Camping'),
     ];
 }
